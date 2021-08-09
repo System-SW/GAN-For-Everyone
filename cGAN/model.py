3fc9921c1294b6dd10e10a5651a92d24374740eb
yslee <yslee.dev@gmail.com>
2021-08-09 20:31:24 +0900

    Add Conditional GAN Trainer

---
+++ cGAN/model.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+
+
+class Discriminator(nn.Module):
+    def __init__(self, channels_img, dim, num_classes, img_size):
+        super(Discriminator, self).__init__()
+        self.img_size = img_size
+        self.disc = nn.Sequential(
+            nn.Conv2d(
+                channels_img + 1, dim,
+                kernel_size=4, stride=2, padding=1
+            ),
+            nn.LeakyReLU(0.2),
+
+            self._block(dim * 1, dim * 2, 4, 2, 1),
+            self._block(dim * 2, dim * 4, 4, 2, 1),
+            self._block(dim * 4, dim * 8, 4, 2, 1),
+
+            nn.Conv2d(dim * 8, 1, kernel_size=4, stride=2, padding=0),
+        )
+        self.embed = nn.Embedding(num_classes, img_size * img_size)
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2),
+        )
+
+    def forward(self, x, labels):
+        embedding = self.embed(labels).view(
+            labels.shape[0], 1, self.img_size, self.img_size)
+        x = torch.cat([x, embedding], dim=1)
+        return self.disc(x)
+
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, channels_img, dim,
+                 num_classes, img_size, embed_size):
+        super(Generator, self).__init__()
+        self.img_size = img_size
+        self.net = nn.Sequential(
+
+            self._block(z_dim + embed_size, dim * 16, 4, 1, 0),  # img: 4x4
+            self._block(dim * 16, dim * 8, 4, 2, 1),  # img: 8x8
+            self._block(dim * 8, dim * 4, 4, 2, 1),  # img: 16x16
+            self._block(dim * 4, dim * 2, 4, 2, 1),  # img: 32x32
+            nn.ConvTranspose2d(
+                dim * 2, channels_img, kernel_size=4, stride=2, padding=1
+            ),  # Output: N x channels_img x 64 x 64
+            nn.Tanh(),
+        )
+        self.embed = nn.Embedding(num_classes, embed_size)
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+
+    def forward(self, x, labels):
+        embedding = self.embed(labels).unsqueeze(2).unsqueeze(3)
+        x = torch.cat([x, embedding], dim=1)
+        return self.net(x)
+
+
+def initialize_weights(model):
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
---
+++ cGAN/train.py
@@ -0,0 +1,120 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from tqdm import tqdm
+
+import hyperparameters as hp
+from dataset import Dataset
+from model import Discriminator, Generator, initialize_weights
+from opt import Template
+
+
+class ConditionalGAN(Template):
+    def __init__(self):
+        super().__init__(
+            device=hp.DEVICE, seed=hp.SEED, model_name='cGAN')
+
+        self.dataset = Dataset(hp.DATASET)
+        self.gen = Generator(hp.NOISE_DIM, hp.IMAGE_CHANNELS,
+                             hp.GEN_DIM, hp.NUM_CLASSES,
+                             hp.IMAGE_SIZE, hp.EMBEDDING_SIZE).to(self.device)
+        self.disc = Discriminator(
+            hp.IMAGE_CHANNELS, hp.DISC_DIM, hp.NUM_CLASSES, hp.IMAGE_SIZE).to(self.device)
+        initialize_weights(self.gen)
+        initialize_weights(self.disc)
+
+        self.FIXED_NOISE = torch.randn(32, hp.NOISE_DIM, 1, 1,
+                                       device=self.device)
+        # optimizer
+        self.opt_gen = optim.Adam(self.gen.parameters(),
+                                  lr=hp.LEARNING_RATE,
+                                  betas=hp.BETAS)
+        self.opt_disc = optim.Adam(self.disc.parameters(),
+                                   lr=hp.LEARNING_RATE,
+                                   betas=hp.BETAS)
+
+        self.scaler_gen = torch.cuda.amp.GradScaler()
+        self.scaler_dis = torch.cuda.amp.GradScaler()
+        self.bce_loss = nn.BCEWithLogitsLoss()
+
+    def train(self):
+        loader = self.dataset.create_dataloader(
+            hp.BATCH_SIZE)
+        self.gen.train()
+        self.disc.train()
+
+        # Training
+        for epoch in range(hp.NUM_EPOCHS):
+            pbar = tqdm(enumerate(loader), total=len(loader))
+            for batch_idx, (real, labels) in pbar:
+                real = real.to(self.device)
+                labels = labels.to(self.device)
+                batch_size = real.shape[0]
+                noise = torch.randn(
+                    batch_size, hp.NOISE_DIM, 1, 1, device=self.device)
+
+                with torch.cuda.amp.autocast():
+                    fake = self.gen(noise, labels)
+                    disc_real = self.disc(real, labels).reshape(-1)
+                    lossD_real = self.bce_loss(
+                        disc_real, torch.ones_like(disc_real))
+                    disc_fake = self.disc(fake.detach(), labels).reshape(-1)
+                    lossD_fake = self.bce_loss(
+                        disc_fake, torch.zeros_like(disc_fake))
+                    lossD = (lossD_fake + lossD_real) / 2
+
+                self.disc.zero_grad()
+                self.scaler_dis.scale(lossD).backward()
+                self.scaler_dis.step(self.opt_disc)
+                self.scaler_dis.update()
+
+                # Train Generator
+                with torch.cuda.amp.autocast():
+                    output = self.disc(fake, labels).reshape(-1)
+                    lossG = self.bce_loss(output,
+                                          torch.ones_like(output))
+
+                self.gen.zero_grad()
+                self.scaler_gen.scale(lossG).backward()
+                self.scaler_gen.step(self.opt_gen)
+                self.scaler_gen.update()
+
+                with torch.no_grad():
+                    if batch_idx % hp.LOG_INTERVAL == 0:
+                        self.tb.add_scalar(
+                            'TRAIN/Real Prob', self.gan_prob(disc_real).item(),
+                            global_step=self.itr)
+                        self.tb.add_scalar(
+                            'TRAIN/Fake Prob', self.gan_prob(disc_fake).item(),
+                            global_step=self.itr)
+
+                    if batch_idx % hp.TEST_INTERVAL == 0:
+                        pbar.set_description_str(
+                            f'Epoch[{epoch+1} / {hp.NUM_EPOCHS}], '
+                            f'real:{self.gan_prob(disc_real).item(): .2f}, '
+                            f'fake:{self.gan_prob(disc_fake).item(): .2f}, '
+                            f'lossD:{lossD.item(): .2f}, '
+                            f'lossG:{lossG.item(): .2f} ')
+                        self.test(real, labels)
+                self.itr += 1
+
+    def test(self, real, labels):
+        labels = labels[:32]
+        fake = self.gen(self.FIXED_NOISE, labels)
+        img_grid_fake = torchvision.utils.make_grid(
+            fake, normalize=True)
+        img_grid_real = torchvision.utils.make_grid(
+            real[:32], normalize=True)
+        self.tb.add_image(
+            "Fake Images", img_grid_fake,
+            global_step=self.itr
+        )
+        self.tb.add_image(
+            "Real Images", img_grid_real,
+            global_step=self.itr
+        )
+
+
+if __name__ == '__main__':
+    trainer = ConditionalGAN().train()
