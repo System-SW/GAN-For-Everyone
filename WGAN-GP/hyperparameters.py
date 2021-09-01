a84e2910f4006ba00b4ff97d29cf4055d0db96b9
yslee <yslee.dev@gmail.com>
2021-09-01 21:55:11 +0900

    Refactor WGAN-GP Trainer

--- WGAN-GP/hyperparameters.py
+++ WGAN-GP/hyperparameters.py
@@ -1,29 +1,39 @@
 import torch
+from dataset import *
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 SEED = 2333
+RESTORE_CKPT_PATH = None
 
 
 class DATASET_MNIST:
-    CAT = "mnist"
-    DIR = "../dataset/"
+    DS = MnistDataset
+    DIR = "../DATASET/"
     IMAGE_SIZE = 64
     IMAGE_CHANNELS = 1
 
 
-class DATASET_CELABA(DATASET_MNIST):
-    CAT = "celaba"
-    DIR = ""
-    IMAGE_SIZE = 64
+class DATASET_FMNIST(DATASET_MNIST):
+    DS = FashionMNISTDataset
+
+
+class DATASET_CIFAR10(DATASET_MNIST):
+    DS = Cifar10Dataset
     IMAGE_CHANNELS = 3
 
 
-DATASET = DATASET_MNIST
+class DATASET_CELABA(DATASET_CIFAR10):
+    DS = CelebADataset
+    DIR = "../DATASET/celebA"
+
+
+DATASET = DATASET_FMNIST
 IMAGE_SIZE = DATASET.IMAGE_SIZE
 IMAGE_CHANNELS = DATASET.IMAGE_CHANNELS
 
 
 BATCH_SIZE = 128
+SAMPLE_SIZE = 10 ** 2
 NOISE_DIM = 100
 GEN_DIM = 16
 DISC_DIM = 16
